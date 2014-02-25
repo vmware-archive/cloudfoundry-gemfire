b397c25d29462fa0e26126289283af68edaa9708
azwickey-pivotal <azwickey@gopivotal.com>
2014-02-25 11:14:55 -0500

    added UI to app

--- src/main/java/com/pivotal/cloudfoundry/example/HomeController.java
+++ src/main/java/com/pivotal/cloudfoundry/example/HomeController.java
@@ -1,5 +1,8 @@
 package com.pivotal.cloudfoundry.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +25,12 @@ public class HomeController {
 	
 	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
 	@ResponseBody
-	public Object get(@PathVariable String id) {
+	public Map<String,String> get(@PathVariable String id) {
 		LOG.info("Getting value for key [" + id + "] from Gemfire");
-		return _template.get(id);
+		Map<String,String> result = new HashMap<>();
+		String value = _template.get(id);
+		if(value != null) result.put(id, value);
+		return result;
 	}
 	
 	@RequestMapping(value = "/put/{id}/{value}", method = RequestMethod.GET)
@@ -32,6 +38,17 @@ public class HomeController {
 	public void put(@PathVariable String id, @PathVariable String value) {
 		LOG.info("Putting value [" + value + "]] for key [" + id + "] from Gemfire");
 		_template.put(id,  value);
+	}
+	
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String,String> getAll() {
+		LOG.info("Getting all values");
+		Map<String,String> result = new HashMap<>();
+		for(Object key : _template.getRegion().keySetOnServer()) {
+			result.put(key.toString(), (String) _template.get(key));
+		}
+		return result;
 	}
 	
 }
